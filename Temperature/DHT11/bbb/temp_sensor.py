fe74862c5d1a3d464ecc24b7e2cd9f1ec7325963
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2015-04-07 04:54:38 +0800

    Added BBB code for DHT11

---
+++ Temperature/DHT11/bbb/temp_sensor.py
@@ -0,0 +1,10 @@
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+
+sensor_pin = 'P8_11'
+
+hum, temp = Adafruit_DHT.read_retry(sensor, sensor_pin)
+
+if hum is not None and temp is not None:
+  print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, hum)
